20a552e87b63bf6a278d3a1f1805632858411227
nalbeito <nalbeito@s34211.local>
2019-02-14 14:59:33 -0600

    Handling form data

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.get("/credit", (req, res) => res.render('account', { account: accounts.credi
 
 app.get("/profile", (req, res) => res.render('profile', { user: users[0] }));
 
-app.get("/tranfer", (req, res) => res.render('transfer'));
+app.get("/transfer", (req, res) => res.render('transfer'));
 app.post("/transfer", (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
